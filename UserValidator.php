965a514dd243471e504b072d536fb070bad6b744
shepard153 <kamil.kosmider1@gmail.com>
2024-09-05 14:07:33 +0200

    Fix spacing

--- UserValidator.php
+++ UserValidator.php
@@ -1,11 +1,11 @@
 <?php
+
 declare(strict_types=1);
 
 class UserValidator
 {
     private const EMAIL_PATTERN = '/^[\w.-]+@([\w-])+\.+[\w-]{2,}/';
     private const PASSWORD_PATTERN = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/';
-
 
     public function validateEmail(string $email): bool
     {
